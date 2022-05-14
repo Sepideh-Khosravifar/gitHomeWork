b678fb5c9d76cf0730efae17c4ebdd7130aa2a34
sisikhosravi <sisikhosravi2021@gmail.com>
2022-05-14 17:23:33 -0400

    2nd commit

--- src/test/java/GITHW1.java
+++ src/test/java/GITHW1.java
@@ -2,7 +2,7 @@ public class GITHW1 {
 
     public static void main(String[] args) {
 
-        for (int i = 1; i <= 100; i++) {
+        for (int i = 1; i <= 50; i++) {
 
             System.out.println(i);
         }
